04b0c9e7501fedffe32385999e5fb56047b4ae13
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-09-05 14:31:15 -0300

    flutter init

--- serv_facil/lib/main.dart
+++ serv_facil/lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:serv_facil/provider/user_provider.dart';
-import 'package:serv_facil/screens/home.dart';
 import 'package:serv_facil/screens/login.dart';
-import 'package:serv_facil/screens/register.dart';
 import 'package:serv_facil/theme/light_theme.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => UserProvider(),
-      child: const MainApp(),
-    ),
-  );
+  runApp(const MainApp());
 }
 
 final navigatorKey = GlobalKey<NavigatorState>();
@@ -24,13 +15,11 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: LightTheme().theme,
       navigatorKey: navigatorKey,
       initialRoute: '/login',
-      theme: LightTheme().theme,
       routes: {
         '/login': (context) => const LoginScreen(),
-        '/home': (context) => const HomeScreen(),
-        '/register': (context) => const RegisterScreen(),
       },
     );
   }
--- serv_facil/lib/theme/light_theme.dart
+++ serv_facil/lib/theme/light_theme.dart
@@ -2,15 +2,7 @@ import 'package:flutter/material.dart';
 
 class LightTheme {
   final ThemeData _theme = ThemeData(
-    brightness: Brightness.light,
-    colorScheme: const ColorScheme.light(
-      onPrimary: Color(0xFF000000),
-      primary: Color(0xFF233c4c),
-      secondary: Color(0xFF32698f),
-      tertiary: Color(0xFF1a9f8e),
-      onTertiary: Color(0xFFFFFFFF),
-      surface: Color(0xFFFFFFFF),
-    )
+    brightness: Brightness.light
   );
 
   ThemeData get theme => _theme;
